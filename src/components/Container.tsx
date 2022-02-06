9ff201e29d0c67b2adf17982364c4f0a0e165897
Rishabh Kumar <rsbkme@gmail.com>
2022-02-06 19:52:01 +0530

    add padding to container

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -4,5 +4,7 @@ export const Container: FC<{ className: string }> = ({
   children,
   className,
 }) => {
-  return <div className={"lg:max-w-4xl mx-auto " + className}>{children}</div>;
+  return (
+    <div className={"lg:max-w-4xl mx-auto px-4 " + className}>{children}</div>
+  );
 };
