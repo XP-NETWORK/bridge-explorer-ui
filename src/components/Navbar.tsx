830074480e120549cd4b6701d100a11830c1e58e
Rishabh Kumar <rsbkme@gmail.com>
2022-02-07 17:35:09 +0530

    make navbar responsive

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,21 +1,35 @@
-import { FC } from "react";
+import { FC, useEffect, useRef } from "react";
 import { Link, NavLink } from "react-router-dom";
 import { Container } from "./Container";
 import logo from "../assets/img/logo.svg";
+import { useToggle } from "../hooks/use-toggle";
 
 export const Navbar = () => {
+  const [isMenuOpen, toggleMenu] = useToggle(false);
+  const menu = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    menu.current?.classList.toggle("hidden");
+  }, [isMenuOpen]);
+
   return (
     <nav className="bg-white shadow">
-      <Container className="items-stretch sm:flex justify-between px-4 border-red-400">
+      <Container className="relative items-stretch flex flex-col sm:flex-row gap-y-4 justify-between px-4 border-red-400">
         <Link to="/">
           <img src={logo} alt="logo" />
         </Link>
-        <div className="sm:flex gap-x-4">
+        <div ref={menu} className="flex sm:flex flex-col sm:flex-row gap-x-4">
           <NavItem to="/">Bridige</NavItem>
           <NavItem to="/network">Network</NavItem>
           <NavItem to="/dashboard">Dashboard</NavItem>
           <NavItem to="/explorer">Explorer</NavItem>
         </div>
+        <button
+          className="absolute right-4 top-7 sm:hidden"
+          onClick={toggleMenu}
+        >
+          MENU
+        </button>
       </Container>
     </nav>
   );
