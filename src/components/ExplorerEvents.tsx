1b6b3b3def231da11b37163c7cc59049ccc356d4
Rishabh Kumar <rsbkme@gmail.com>
2022-02-14 19:14:47 +0530

    refactor ExplorerEvents.tsx

--- src/components/ExplorerEvents.tsx
+++ src/components/ExplorerEvents.tsx
@@ -1,4 +1,5 @@
 import { FC, useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import { Container } from "./Container";
 
 export interface IEvent {
@@ -55,15 +56,25 @@ export const ExplorerEvents = () => {
           {events.length ? (
             events.map((event, index) => (
               <tr key={event.id}>
-                <TableData>{index}</TableData>
+                <TableData>{index + 1}</TableData>
                 <TableData>
                   <img
                     className="rounded-lg"
                     src={event?.imgUri || "https://via.placeholder.com/50"}
                     alt=""
+                    width={50}
+                    height={50}
                   />
                 </TableData>
-                <TableData>{event.fromHash || "N/A"}</TableData>
+                <TableData>
+                  <Link
+                    className="text-blue-500"
+                    key={event.id}
+                    to={`/${event.fromHash}`}
+                  >
+                    {event.fromHash || "N/A"}
+                  </Link>
+                </TableData>
                 <TableData>{event.type || "N/A"}</TableData>
                 <TableData>{event.fromChain || "N/A"}</TableData>
                 <TableData>{event.toChain || "N/A"}</TableData>
@@ -92,14 +103,14 @@ export const ExplorerEvents = () => {
 const TableHeading: FC = ({ children }) => (
   <th
     scope="col"
-    className="px-3 py-3 text-left text-xs font-medium text-gray-800 tracking-wider"
+    className="px-3 py-3 text-left max-w-xs text-xs font-medium text-gray-800 tracking-wider"
   >
     {children}
   </th>
 );
 
 const TableData: FC = ({ children }) => (
-  <td className="px-3 py-4 whitespace-nowrap max-w-sm text-ellipsis overflow-hidden text-sm text-gray-500">
+  <td className="px-3 py-4 whitespace-nowrap max-w-xs text-ellipsis overflow-hidden text-sm text-gray-500">
     {children}
   </td>
 );
