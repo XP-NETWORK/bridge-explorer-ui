54688193b8446ed3d43d07f6236aadf67da19932
Rishabh Kumar <rsbkme@gmail.com>
2022-02-15 19:33:26 +0530

    add inter font

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,10 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
+    fontFamily: {
+      sans: ["Inter", "sans-serif"],
+      body: ["Inter", "sans-serif"],
+    },
   },
   plugins: [],
 };
