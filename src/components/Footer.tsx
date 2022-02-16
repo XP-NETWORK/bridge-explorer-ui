6f9668189ef2a7d3528855f7c40e2d0b6c65c649
Rishabh Kumar <rsbkme@gmail.com>
2022-02-16 16:40:22 +0530

    add footer links

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,3 +1,4 @@
+import { FC } from "react";
 import { Container } from "./Container";
 
 export const Footer = () => {
@@ -7,44 +8,77 @@ export const Footer = () => {
         <div className="flex flex-col md:flex-row gap-y-10 justify-between">
           <div className="flex flex-col gap-4">
             <h1 className="font-medium text-white">Products</h1>
-            <a href="/">Cross-Chain NFT Bridge</a>
-            <a href="/">NFT Bridge Widget</a>
-            <a href="/">XPNET-JS API</a>
+            <FooterLink href="https://bridge.xp.network/">
+              Cross-Chain NFT Bridge
+            </FooterLink>
+            <FooterLink disabled href="/">
+              NFT Bridge Widget
+            </FooterLink>
+            <FooterLink href="https://xp.network/api/">XPNET-JS API</FooterLink>
           </div>
           <div className="flex flex-col gap-4">
             <h1 className="font-medium text-white">Resources</h1>
-            <a href="/">Whitepaper</a>
-            <a href="/">Wiki</a>
-            <a href="/">Docs</a>
-            <a href="/">Roadmap</a>
-            <a href="/">GitHub</a>
+            <FooterLink href="https://xp.network/whitepaper/">
+              Whitepaper
+            </FooterLink>
+            <FooterLink href="https://docs.xp.network/">Docs</FooterLink>
+            <FooterLink href="https://docs.xp.network/docs/roadmap/">
+              Roadmap
+            </FooterLink>
+            <FooterLink href="https://github.com/xp-network/">
+              GitHub
+            </FooterLink>
           </div>
           <div className="flex flex-col gap-4">
             <h1 className="font-medium text-white">XPNET</h1>
-            <a href="/">Token</a>
-            <a href="/">Staking</a>
+            <FooterLink disabled href="/">
+              Token
+            </FooterLink>
+            <FooterLink href="https://stake.xp.network/stake">
+              Staking
+            </FooterLink>
           </div>
           <div className="flex flex-col gap-4">
             <h1 className="font-medium text-white">XP.NETWORK</h1>
-            <a href="/">Team</a>
-            <a href="/">Blog</a>
-            <a href="/">Brand Assets</a>
+            <FooterLink href="https://xp.network/team/">Team</FooterLink>
+            <FooterLink href="https://blog.xp.network/">Blog</FooterLink>
+            <FooterLink href="https://drive.google.com/drive/folders/1i8evWmyH_8APiDDO89depEw_8JnDSACK">
+              Brand Assets
+            </FooterLink>
           </div>
           <div className="flex flex-col gap-4">
             <h1 className="font-medium text-white">Community</h1>
-            <a href="/">Ambassadors ship</a>
-            <a href="/">Global Chanels</a>
+            <FooterLink disabled href="/">
+              Ambassadors ship
+            </FooterLink>
+            <FooterLink href="https://xp.network/community/">
+              Global Chanels
+            </FooterLink>
           </div>
           <div className="flex flex-col gap-4">
             <h1 className="font-medium text-white">Follow</h1>
-            <a href="/">Twitter</a>
-            <a href="/">Telegram</a>
-            <a href="/">Reddit</a>
-            <a href="/">Linkedin</a>
-            <a href="/">Facebook</a>
-            <a href="/">Discord</a>
-            <a href="/">Bitclout</a>
-            <a href="/">Instagram</a>
+            <FooterLink href="https://twitter.com/xpnetwork_">
+              Twitter
+            </FooterLink>
+            <FooterLink href="https://t.me/xp_network">Telegram</FooterLink>
+            <FooterLink href="https://www.reddit.com/r/XP_network/">
+              Reddit
+            </FooterLink>
+            <FooterLink href="https://www.linkedin.com/company/xpnetwork/mycompany/">
+              Linkedin
+            </FooterLink>
+            <FooterLink href="https://www.facebook.com/XPNETWORKOFFICIAL/">
+              Facebook
+            </FooterLink>
+            <FooterLink href="https://discord.com/invite/g3vkcsmd38">
+              Discord
+            </FooterLink>
+            <FooterLink href="https://bitclout.com/u/XPnetwork">
+              Bitclout
+            </FooterLink>
+            <FooterLink href="https://www.instagram.com/xp_network/">
+              Instagram
+            </FooterLink>
           </div>
         </div>
         <hr className="border-zinc-700 my-10" />
@@ -54,5 +88,21 @@ export const Footer = () => {
         </div>
       </Container>
     </footer>
+  );
+};
+
+const FooterLink: FC<{ disabled?: boolean; href: string }> = ({
+  children,
+  disabled = false,
+  href,
+}) => {
+  return (
+    <a
+      target="_blank"
+      href={href}
+      className={disabled ? "pointer-events-none text-zinc-500" : ""}
+    >
+      {children}
+    </a>
   );
 };
