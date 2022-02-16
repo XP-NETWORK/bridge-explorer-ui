a17fe4e6e4177fd3c4bf3aa0ca2e406d71a33be1
Rishabh Kumar <rsbkme@gmail.com>
2022-02-16 17:58:46 +0530

    add font-medium to subtitle in cards

--- src/components/ExplorerCard.tsx
+++ src/components/ExplorerCard.tsx
@@ -12,7 +12,7 @@ export const ExplorerCard: FC<Props> = ({ title, subtitle, icon }) => {
     <div className="shadow relative overflow-hidden rounded-lg font-poppins bg-white w-full p-5">
       <img src={icon} alt="icon" />
       <h1 className="text-3xl font-medium mt-3">{title}</h1>
-      <h2 className="text-md mt-2">{subtitle}</h2>
+      <h2 className="text-md font-medium mt-2">{subtitle}</h2>
       <img className="absolute right-0 top-0" src={ExplorerCardBg} alt="" />
     </div>
   );
