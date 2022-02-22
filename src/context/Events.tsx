2f39c036c5fc6d6de489ff96ef959c3583f82ae0
AlexeyAdoniev <Alexeya@xp.network>
2022-02-23 01:07:01 +0200

    sockets sett

--- src/context/Events.tsx
+++ src/context/Events.tsx
@@ -8,20 +8,24 @@ import {
 } from "react";
 import { IEvent } from "../components/ExplorerEvents";
 
+const url = "http://localhost:3100/"; //'https://dev-explorer-api.herokuapp.com/'
 interface IEventsContext {
   events: IEvent[];
   setChainName: (chainName: string) => void;
 }
 
 export const EventsContext = createContext<IEventsContext | null>(null);
-
 export const EventsProvider: FC = ({ children }) => {
   const [events, setEvents] = useState<IEvent[]>([]);
   const [chainName, setChainName] = useState("");
 
   useEffect(() => {
+    //socket.emit("test");
+  }, [events]);
+
+  useEffect(() => {
     if (chainName.length) {
-      fetch("https://dev-explorer-api.herokuapp.com/?chainName=" + chainName)
+      fetch(`${url}?chainName=` + chainName)
         .then((res) => res.json())
         .then(async (data: IEvent[]) => {
           const newEvents = data.map(async (data) => {
@@ -32,7 +36,7 @@ export const EventsProvider: FC = ({ children }) => {
           setEvents(await Promise.all(newEvents));
         });
     } else {
-      fetch("https://dev-explorer-api.herokuapp.com/")
+      fetch(`${url}`)
         .then((res) => res.json())
         .then(async (data: IEvent[]) => {
           const newEvents = data.map(async (data) => {
@@ -46,12 +50,10 @@ export const EventsProvider: FC = ({ children }) => {
               return { imgUri: "", ...data };
             }
           });
-
           setEvents(await Promise.all(newEvents));
         });
     }
   }, [chainName]);
-
   return (
     <EventsContext.Provider value={{ events, setChainName }}>
       {children}
--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,13 @@ import { App } from "./App";
 import "./index.css";
 import "./components/elements/elements.css";
 import { BrowserRouter } from "react-router-dom";
+import io from "socket.io-client";
+
+console.log("d");
+const socket = io("ws://localhost:3100", { path: "/socket.io" });
+socket.on("msg", (data) => {
+  console.log(data);
+});
 
 ReactDOM.render(
   <React.StrictMode>
