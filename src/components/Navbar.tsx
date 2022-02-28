0c7cb6f6b57f36cf79b183f78b432315c8de4f04
Rishabh Kumar <rsbkme@gmail.com>
2022-02-28 17:05:29 +0530

    style: change navbar menu style

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,6 +3,7 @@ import { NavLink } from "react-router-dom";
 import { Container } from "./Container";
 import logo from "../assets/img/logo.svg";
 import { useToggle } from "../hooks/use-toggle";
+import MenuIcon from "../assets/icons/menu.svg";
 
 export const Navbar = () => {
   const [isMenuOpen, toggleMenu] = useToggle(false);
@@ -14,17 +15,17 @@ export const Navbar = () => {
 
   return (
     <nav className="bg-white shadow-md">
-      <Container className="relative flex-shrink flex flex-col sm:flex-row gap-y-4 justify-between px-4 border-red-400">
+      <Container className="relative flex-shrink  flex flex-col sm:flex-row gap-y-4 justify-between px-4 border-red-400">
         <a target="_blank" href="https://xp.network" className="max-w-fit">
           <img src={logo} alt="logo" />
         </a>
         <div
           ref={menu}
-          className="flex sm:flex flex-col justify-between sm:flex-row gap-x-4"
+          className="absolute overflow-hidden sm:static bg-white right-0  top-16 z-40 rounded-xl flex sm:flex flex-col justify-between sm:flex-row gap-x-4"
         >
           <NavItem to="/">Explorer</NavItem>
           <a
-            className="sm:flex text-[#030303] items-end font-normal tracking-wide hover:tracking-[0.0175em] text-center hover:font-medium text-[15px] pb-2.5 max-w-fit sm:border-t-2 border-transparent"
+            className="sm:flex p-4 w-full min-w-[14rem] z-20  hover:bg-slate-100 sm:min-w-fit sm:p-0 sm:hover:bg-white  text-[#030303] items-end font-normal tracking-wide hover:tracking-[0.0175em] hover:font-medium text-[15px] sm:pb-2.5 max-w-fit sm:border-t-2 border-transparent"
             href="https://bridge.xp.network/"
             target="_blank"
           >
@@ -37,7 +38,7 @@ export const Navbar = () => {
           className="absolute right-4 top-7 sm:hidden"
           onClick={toggleMenu}
         >
-          MENU
+          <img src={MenuIcon} alt="menu icon" />
         </button>
       </Container>
     </nav>
@@ -47,7 +48,7 @@ export const Navbar = () => {
 const NavItem: FC<{ to: string }> = ({ children, to }) => {
   return (
     <NavLink
-      className="sm:flex text-[#030303] items-end font-normal tracking-wide hover:tracking-[0.0175em] text-center hover:font-medium text-[15px] pb-2.5 max-w-fit sm:border-t-2 border-transparent"
+      className="sm:flex p-4 w-full min-w-[14rem] z-20 sm:min-w-fit sm:p-0 sm:hover:bg-white  hover:bg-slate-100 text-[#030303] items-end font-normal tracking-wide sm:pb-2.5  hover:tracking-[0.0175em] hover:font-medium text-[15px] max-w-fit sm:border-t-2 border-transparent"
       to={to}
     >
       {children}
