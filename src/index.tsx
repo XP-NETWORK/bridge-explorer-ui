89ea727fcd6b00401e5ee08d311abce3789f19f8
AlexeyAdoniev <Alexeya@xp.network>
2022-03-01 20:06:15 +0200

    index

--- src/index.tsx
+++ src/index.tsx
@@ -8,11 +8,8 @@ import { BrowserRouter } from "react-router-dom";
 import { ServiceProvider } from "../src/context/ServcieProvder";
 import io from "socket.io-client";
 
-/*const socket = io("ws://dev-explorer-api.herokuapp.com", {
-  path: "/socket.io",
-});*/
 
-const socket = io("ws://dev-explorer-api.herokuapp.com", {
+const socket = io("ws://localhost:3100", {//http://localhost:3100/  // ws://dev-explorer-api.herokuapp.com
   path: "/socket.io",
 });
 
