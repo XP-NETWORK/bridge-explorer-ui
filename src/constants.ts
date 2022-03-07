cdfb71c35d15b563cce714db19d1a108e9911528
Rishabh Kumar <rsbkme@gmail.com>
2022-03-07 19:55:43 +0530

    add testnet constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,45 +1,44 @@
-export const currency:any = {
-    '4': 'BNB',
-    '19': 'VLX',
-    '14': 'xDAI',
-    '2': 'EGLD',
-    '20': 'IOTX',
-    '16': 'Fuse',
-    '6': 'AVAX',
-    '21': 'AETH',
-    '7': 'MATIC',
-    '5': 'ETH'
-
-}
+export const currency: any = {
+  "4": "BNB",
+  "19": "VLX",
+  "14": "xDAI",
+  "2": "EGLD",
+  "20": "IOTX",
+  "16": "Fuse",
+  "6": "AVAX",
+  "21": "AETH",
+  "7": "MATIC",
+  "5": "ETH",
+};
 
 export const txExplorers: any = {
-    '4': 'https://bscscan.com/tx/',
-    '19': 'https://explorer.velas.com/tx/',
-    '14': 'https://blockscout.com/xdai/mainnet/tx/',
-    '2': 'https://explorer.elrond.com/transactions/',
-    '20': 'https://iotexscan.io/tx/',
-    '6': 'https://snowtrace.io/tx/',
-    '16': 'https://explorer.fuse.io/tx/',
-    '21': 'https://explorer.mainnet.aurora.dev/tx/',
-    '7': 'https://polygonscan.com/tx/',
-    '5': 'https://ethplorer.io/tx/'
-}
+  "4": "https://bscscan.com/tx/",
+  "19": "https://explorer.velas.com/tx/",
+  "14": "https://blockscout.com/xdai/mainnet/tx/",
+  "2": "https://explorer.elrond.com/transactions/",
+  "20": "https://iotexscan.io/tx/",
+  "6": "https://snowtrace.io/tx/",
+  "16": "https://explorer.fuse.io/tx/",
+  "21": "https://explorer.mainnet.aurora.dev/tx/",
+  "7": "https://polygonscan.com/tx/",
+  "5": "https://ethplorer.io/tx/",
+};
 
 export const addressExplorers: any = {
-    '4': 'https://bscscan.com//address/',
-    '19': 'https://explorer.velas.com/address/',
-    '14': 'https://blockscout.com/xdai/mainnet/address/',
-    '2': 'https://explorer.elrond.com/accounts/',
-    '20': 'https://iotexscan.io/address/',
-    '6': 'https://snowtrace.io/address/',
-    '16': 'https://explorer.fuse.io/address/',
-    '21': 'https://explorer.mainnet.aurora.dev/address/',
-    '7': 'https://polygonscan.com/address/',
-    '5': 'https://ethplorer.io/address/'
-}
+  "4": "https://bscscan.com//address/",
+  "19": "https://explorer.velas.com/address/",
+  "14": "https://blockscout.com/xdai/mainnet/address/",
+  "2": "https://explorer.elrond.com/accounts/",
+  "20": "https://iotexscan.io/address/",
+  "6": "https://snowtrace.io/address/",
+  "16": "https://explorer.fuse.io/address/",
+  "21": "https://explorer.mainnet.aurora.dev/address/",
+  "7": "https://polygonscan.com/address/",
+  "5": "https://ethplorer.io/address/",
+};
 
-export const url = 'https://dev-explorer-api.herokuapp.com/';
-export const socketUrl = 'wss://dev-explorer-api.herokuapp.com';
+export const url = "https://testnet-bridge-explorer.herokuapp.com/";
+export const socketUrl = "wss://testnet-bridge-explorer.herokuapp.com";
 
 //export const url = 'http://localhost:3100/';
 //export const socketUrl = 'ws://localhost:3100';
