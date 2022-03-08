6129c7b0490a8a6ba35afd44ffd6bb2f8aa70a51
Rishabh Kumar <rsbkme@gmail.com>
2022-03-08 17:35:07 +0530

    add pending tab

--- src/components/ExplorerEvents.tsx
+++ src/components/ExplorerEvents.tsx
@@ -1,4 +1,11 @@
-import { FC, ReactNode, useEffect, useState, useRef, MutableRefObject } from "react";
+import {
+  FC,
+  ReactNode,
+  useEffect,
+  useState,
+  useRef,
+  MutableRefObject,
+} from "react";
 import { Link } from "react-router-dom";
 import { Container } from "./Container";
 import { useContext } from "react";
@@ -10,7 +17,7 @@ import { ethers } from "ethers";
 import { currency } from "../constants";
 import ReactTooltip from "react-tooltip";
 import moment from "moment";
-import scrollUp from '../assets/img/collapse.svg'
+import scrollUp from "../assets/img/collapse.svg";
 
 export interface IEvent {
   id: string;
@@ -33,144 +40,169 @@ export interface IEvent {
   imgUri?: string;
 }
 
-
-
-export const ExplorerEvents = () => {
-  // @ts-ignore
-  const { events } = useContext(EventsContext);
+export const ExplorerEvents: FC<{ chainName?: string }> = ({
+  chainName = "",
+}) => {
+  const eventsContext = useContext(EventsContext);
 
   useEffect(() => {
-    console.log(events);
-  }, [events]);
+    eventsContext?.setChainName(chainName);
+    console.log(eventsContext?.events);
+    console.log("chainName", chainName);
+  }, [chainName]);
 
   let scrollBtn = useRef<any>(null);
 
   const scrollHandler = function () {
-    if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight - 500) {
-      scrollBtn.current.style.bottom = '500px';
+    if (
+      window.innerHeight + window.scrollY >=
+      document.body.offsetHeight - 500
+    ) {
+      scrollBtn.current.style.bottom = "500px";
     } else {
-      scrollBtn.current.style.bottom = '30px';
+      scrollBtn.current.style.bottom = "30px";
     }
-  }
+  };
 
   useEffect(() => {
-     if (events.length) {
-      scrollBtn.current.style.visibility = 'visible';
-      window.addEventListener('scroll', scrollHandler)
-      return () => window.removeEventListener('scroll', scrollHandler)
-
-     }
-     
-      
-  }, [events])
+    if (eventsContext?.events.length) {
+      scrollBtn.current.style.visibility = "visible";
+      window.addEventListener("scroll", scrollHandler);
+      return () => window.removeEventListener("scroll", scrollHandler);
+    }
+  }, [eventsContext?.events]);
 
   return (
     <>
-    <Container className="mt-5 px-0 sm:px-4 overflow-x-auto tableWrapper">
-    <img src={scrollUp} alt="scrollUp" className="scrollTopBtn" ref={scrollBtn} onClick={(e) =>  {
-        setTimeout(() => window.scrollTo({top: 10, behavior: 'smooth'}), 100)
-    }} /> 
-      <table className="min-w-full divide-y border-b divide-gray-200 eventsTable">
- 
-        <thead className="bg-gray-50 ">
-          <tr>
-            <TableHeading>NFT</TableHeading>
-            <TableHeading>Tx Value</TableHeading>
-            {false && <TableHeading>Tx Hash</TableHeading>}
-            <TableHeading>Coin</TableHeading>
-            <TableHeading>Tx Type</TableHeading>
-            <TableHeading>From</TableHeading>
-            <TableHeading>To</TableHeading>
-            <TableHeading>Date</TableHeading>
-            <TableHeading>Status</TableHeading>
-          </tr>
-        </thead>
-        <tbody className="bg-white divide-y  divide-gray-200 overflow-x-scroll">
-      
-          {events.length ? (
-            events.map((event: IEvent) => (
-              <tr key={event.id}>
-                <TableData className="sticky left-0 text-center max-w-[62px] bg-white imgTableData" >
-                <ReactTooltip
-                  effect="solid"
-                  backgroundColor = "#575151"
-                
-                />
-                { event?.status === "Completed" || event?.imgUri ? 
-                  <img
-                    className="rounded-lg"
-                    src={event?.imgUri || ImgBroken}
-                    alt=""
-                    width={38}
-                    height={38}
-                  />:  <div className="lds-ellipsis"><div></div><div></div><div></div><div></div></div>}
-                </TableData>
+      <Container className="mt-5 px-0 sm:px-4 overflow-x-auto tableWrapper">
+        <img
+          src={scrollUp}
+          alt="scrollUp"
+          className="scrollTopBtn"
+          ref={scrollBtn}
+          onClick={(e) => {
+            setTimeout(
+              () => window.scrollTo({ top: 10, behavior: "smooth" }),
+              100
+            );
+          }}
+        />
+        <table className="min-w-full divide-y border-b divide-gray-200 eventsTable">
+          <thead className="bg-gray-50 ">
+            <tr>
+              <TableHeading>NFT</TableHeading>
+              <TableHeading>Tx Value</TableHeading>
+              {false && <TableHeading>Tx Hash</TableHeading>}
+              <TableHeading>Coin</TableHeading>
+              <TableHeading>Tx Type</TableHeading>
+              <TableHeading>From</TableHeading>
+              <TableHeading>To</TableHeading>
+              <TableHeading>Date</TableHeading>
+              <TableHeading>Status</TableHeading>
+            </tr>
+          </thead>
+          <tbody className="bg-white divide-y  divide-gray-200 overflow-x-scroll">
+            {eventsContext?.events.length ? (
+              eventsContext?.events.map((event: IEvent) => (
+                <tr key={event.id}>
+                  <TableData className="sticky left-0 text-center max-w-[62px] bg-white imgTableData">
+                    <ReactTooltip effect="solid" backgroundColor="#575151" />
+                    {event?.status === "Completed" || event?.imgUri ? (
+                      <img
+                        className="rounded-lg"
+                        src={event?.imgUri || ImgBroken}
+                        alt=""
+                        width={38}
+                        height={38}
+                      />
+                    ) : (
+                      <div className="lds-ellipsis">
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                      </div>
+                    )}
+                  </TableData>
 
-                <TableData >
-                  <span className="valueData" data-tip={ethers.utils.formatEther(event.txFees)}>{Number(ethers.utils.formatEther(event.txFees)).toFixed(7).toString()}</span>
-                </TableData>
+                  <TableData>
+                    <span
+                      className="valueData"
+                      data-tip={ethers.utils.formatEther(event.txFees)}
+                    >
+                      {Number(ethers.utils.formatEther(event.txFees))
+                        .toFixed(7)
+                        .toString()}
+                    </span>
+                  </TableData>
 
-                <TableData>
-                {event.fromChain && currency[event.fromChain]}
-                </TableData>
+                  <TableData>
+                    {event.fromChain && currency[event.fromChain]}
+                  </TableData>
 
+                  {false && (
+                    <TableData>
+                      <Link
+                        className="text-[#235EF5]"
+                        key={event.id}
+                        to={`/tx/${event.fromHash}`}
+                      >
+                        {event.fromHash.slice(0, 6)}...
+                        {event.fromHash.slice(-6)}
+                      </Link>
+                    </TableData>
+                  )}
 
-
-             { false &&   <TableData>
-                  <Link
-                    className="text-[#235EF5]"
-                    key={event.id}
-                    to={`/tx/${event.fromHash}`}
-                  >
-                    {event.fromHash.slice(0, 6)}...
-                    {event.fromHash.slice(-6)}
-                  </Link>
-                </TableData>}
-
-
-
-                <TableData><span className="methodDataTable">{event.type || "N/A"}</span></TableData>
-                <TableData>
-                  <div>{event.fromChainName || "N/A"} </div>{" "}
-                  <Link
-                    className="text-[#235EF5]"
-                    key={event.id}
-                    to={`/tx/${event.fromHash}`}
-                  >
-                    {event.fromHash.slice(0, 6)}...
-                    {event.fromHash.slice(-6)}
-                  </Link>
-                </TableData>
-                <TableData>
-                  <div>{event.toChainName || "N/A"}</div>
-                  <Link
-                    className="text-[#235EF5]"
-                    key={event.id}
-                    to={`/tx/${event.fromHash}`}
-                  >
-                    {event?.toHash?.slice(0, 6)}...
-                    {event?.toHash?.slice(-6)}
-                  </Link>
-                </TableData>
-                <TableData>
-                  { moment(event?.createdAt).format("YYYY/MM/DD H:mm") || "N/A"}
-                </TableData>
-                <TableData>
-                  <Status status={event.status} />
-                </TableData>
-              </tr>
-            ))
-          ) : (
-            <LoaderRow/>
-          )}
-       
-        </tbody>
-      </table>
-    </Container></>
+                  <TableData>
+                    <span className="methodDataTable">
+                      {event.type || "N/A"}
+                    </span>
+                  </TableData>
+                  <TableData>
+                    <div>{event.fromChainName || "N/A"} </div>{" "}
+                    <Link
+                      className="text-[#235EF5]"
+                      key={event.id}
+                      to={`/tx/${event.fromHash}`}
+                    >
+                      {event.fromHash.slice(0, 6)}...
+                      {event.fromHash.slice(-6)}
+                    </Link>
+                  </TableData>
+                  <TableData>
+                    <div>{event.toChainName || "N/A"}</div>
+                    <Link
+                      className="text-[#235EF5]"
+                      key={event.id}
+                      to={`/tx/${event.fromHash}`}
+                    >
+                      {event?.toHash?.slice(0, 6)}...
+                      {event?.toHash?.slice(-6)}
+                    </Link>
+                  </TableData>
+                  <TableData>
+                    {moment(event?.createdAt).format("YYYY/MM/DD H:mm") ||
+                      "N/A"}
+                  </TableData>
+                  <TableData>
+                    <Status status={event.status} />
+                  </TableData>
+                </tr>
+              ))
+            ) : (
+              <LoaderRow />
+            )}
+          </tbody>
+        </table>
+      </Container>
+    </>
   );
 };
 
-export const TableHeading: FC<{ className?: string }> = ({ children, className }) => (
+export const TableHeading: FC<{ className?: string }> = ({
+  children,
+  className,
+}) => (
   <th
     scope="col"
     className={
@@ -182,8 +214,10 @@ export const TableHeading: FC<{ className?: string }> = ({ children, className }
   </th>
 );
 
-
-export const TableData: FC<{ className?: string }> = ({ children, className }) => (
+export const TableData: FC<{ className?: string }> = ({
+  children,
+  className,
+}) => (
   <td
     className={
       "px-3 py-4 min-w-[62px] border-0 whitespace-nowrap text-sm text-[#222222] " +
