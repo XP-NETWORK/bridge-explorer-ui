d435bc9e36ae6687f9cbc4aa72a065abe03df0fb
Rishabh Kumar <rsbkme@gmail.com>
2022-03-09 16:13:11 +0530

    fix: status min-width

--- src/components/Status.tsx
+++ src/components/Status.tsx
@@ -7,7 +7,7 @@ import canceledIcon from "../assets/icons/canceled.svg";
 export const Status: FC<{ status?: string }> = ({ status }) => {
   if (status === "Completed")
     return (
-      <div className="flex min-w-[80px] flex-nowrap space-x-1 text-xs text-[#10B67A]">
+      <div className="flex min-w-[5rem] flex-nowrap space-x-1 text-xs text-[#10B67A]">
         <img
           src={completedIcon}
           className="aspect-square"
@@ -20,7 +20,7 @@ export const Status: FC<{ status?: string }> = ({ status }) => {
 
   if (status === "Pending")
     return (
-      <div className="flex space-x-1 text-xs text-[#C058FF]">
+      <div className="flex min-w-[5rem] space-x-1 text-xs text-[#C058FF]">
         <img src={pendingIcon} alt="pending icon" />
         <h1>Pending</h1>
       </div>
@@ -28,7 +28,7 @@ export const Status: FC<{ status?: string }> = ({ status }) => {
 
   if (status === "Failed")
     return (
-      <div className="flex space-x-1 text-xs text-[#C23165]">
+      <div className="flex min-w-[5rem] space-x-1 text-xs text-[#C23165]">
         <img src={failedIcon} alt="failed icon" />
         <h1>Failed</h1>
       </div>
@@ -36,7 +36,7 @@ export const Status: FC<{ status?: string }> = ({ status }) => {
 
   if (status === "Canceled")
     return (
-      <div className="flex space-x-1 text-xs text-[#D7600A]">
+      <div className="flex min-w-[5rem] space-x-1 text-xs text-[#D7600A]">
         <img src={canceledIcon} alt="canceled icon" />
         <h1>Canceled</h1>
       </div>
