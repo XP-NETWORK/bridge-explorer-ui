34216cf5f2d237f6e6f0b7093c7effed19f50eb4
Rishabh Kumar <rsbkme@gmail.com>
2022-03-10 16:19:44 +0530

    fix: tools frontend

--- src/components/Tools.tsx
+++ src/components/Tools.tsx
@@ -1,11 +1,12 @@
+import { FC, useState } from "react";
 import { chains } from "../constants";
 import { Container } from "./Container";
 
 export const Tools = () => {
   return (
     <Container>
-      <div className="grid grid-cols-6 mt-5 gap-10">
-        <div className="col-span-2">
+      <div className="grid grid-cols-1 md:grid-cols-6 mt-5 gap-10">
+        <div className="col-span-4 md:col-span-2">
           <Form />
         </div>
         <div className="col-span-4">
@@ -17,28 +18,35 @@ export const Tools = () => {
 };
 
 const Form = () => {
+  const [txHash, setTxHash] = useState("");
+  const [fromChain, setFromChain] = useState("");
+  const [toChain, setToChain] = useState("");
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(txHash, fromChain, toChain);
+    // TODO: send tx to server
+  };
+
   return (
-    <form>
-      <label className="block  space-y-2">
+    <form onSubmit={handleSubmit}>
+      <div className="block  space-y-2">
         <span>Tx Hash:</span>
-        <input type="text" className="w-full border-gray-200 rounded" />
-      </label>
-      <label className="block mt-5 space-y-2">
-        <span>Departure Chain:</span>
-        <select className="w-full border-gray-200  rounded">
-          {chains.map((chain) => (
-            <option key={chain.name}>{chain.name}</option>
-          ))}
-        </select>
-      </label>
-      <label className="block mt-5 space-y-2">
+        <input
+          value={txHash}
+          onChange={(e) => setTxHash(e.currentTarget.value)}
+          type="text"
+          className="bg-white w-full focus:outline-none select-none border rounded px-4 py-2"
+        />
+      </div>
+      <div className="block mt-5 space-y-2">
         <span>Destination Chain:</span>
-        <select className="w-full border-gray-200  rounded">
-          {chains.map((chain) => (
-            <option key={chain.name}>{chain.name}</option>
-          ))}
-        </select>
-      </label>
+        <Dropdown setSelectedChain={setFromChain} />
+      </div>
+      <div className="block mt-5 space-y-2">
+        <span>Destination Chain:</span>
+        <Dropdown setSelectedChain={setToChain} />
+      </div>
       <button
         type="submit"
         className="block mt-5 w-full p-2 text-center text-white rounded-md bg-[#297EFE]"
@@ -46,6 +54,52 @@ const Form = () => {
         Send
       </button>
     </form>
+  );
+};
+
+const Dropdown: FC<{
+  setSelectedChain: React.Dispatch<React.SetStateAction<string>>;
+}> = ({ setSelectedChain }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [_selectedChain, _setSelectedChain] = useState("");
+
+  return (
+    <div className="relative">
+      <h1
+        className="bg-white flex justify-between items-center select-none border rounded px-4 py-2"
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        <span>{_selectedChain.length ? _selectedChain : "Select Chain"}</span>
+        <svg
+          width="8"
+          height="4"
+          viewBox="0 0 8 4"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path d="M0 -3.49691e-07L4 4L8 0L0 -3.49691e-07Z" fill="#62718A" />
+        </svg>
+      </h1>
+      <ul
+        className={`${
+          isOpen || "hidden"
+        } z-10 rounded absolute w-full py-2 bg-white shadow max-h-72 overflow-y-scroll`}
+      >
+        {chains.map((chain) => (
+          <li
+            onClick={(e) => {
+              _setSelectedChain(e.currentTarget.innerText);
+              setSelectedChain(e.currentTarget.innerText);
+              setIsOpen(false);
+            }}
+            className="py-2 px-4 select-none cursor-pointer hover:bg-slate-100"
+            key={chain.name}
+          >
+            {chain.name}
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 };
 
