a5ac32cb836e95209baac614049e3fc9a576a937
Rishabh Kumar <rsbkme@gmail.com>
2022-03-10 19:50:09 +0530

    change font size to 32 px

--- src/components/ExplorerCard.tsx
+++ src/components/ExplorerCard.tsx
@@ -11,9 +11,18 @@ export const ExplorerCard: FC<Props> = ({ title, subtitle, icon }) => {
   return (
     <div className="shadow-[0_0px_10px_0px_rgba(0,0,0,0.2)] relative overflow-hidden rounded-lg font-poppins bg-white w-full p-5 explorerCard">
       <img src={icon} alt="icon" />
-      <h1 className="text-3xl font-medium mt-3" style={{visibility: title? 'visible': 'hidden'}}>{title}</h1>
+      <h1
+        className="text-[2rem] font-medium mt-3"
+        style={{ visibility: title ? "visible" : "hidden" }}
+      >
+        {title}
+      </h1>
       <h2 className="text-md font-medium mt-2">{subtitle}</h2>
-      <img className="absolute right-0 top-0 dotsImg" src={ExplorerCardBg} alt="" />
+      <img
+        className="absolute right-0 top-0 dotsImg"
+        src={ExplorerCardBg}
+        alt=""
+      />
     </div>
   );
 };
