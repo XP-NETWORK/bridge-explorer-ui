4c15453bf79928990ddabd7d7aa9a30b502b9776
Rishabh Kumar <rsbkme@gmail.com>
2022-03-13 18:11:34 +0530

    fix copy icon

--- src/components/Details/CopyWithTooltop.tsx
+++ src/components/Details/CopyWithTooltop.tsx
@@ -1,23 +1,29 @@
-
-
 import CopyIcon from "../../assets/icons/copy.svg";
 import { IEvent } from "../ExplorerEvents";
- 
-const CopyWithTooltip = ({copyValue, copyIdx, copyProps: {tooltipCopy, tooltips, setTooltipCopy}}: {
-  copyValue:string | undefined, copyProps: {tooltipCopy:number, tooltips:any, setTooltipCopy:Function}, copyIdx:number
+
+const CopyWithTooltip = ({
+  copyValue,
+  copyIdx,
+  copyProps: { tooltipCopy, tooltips, setTooltipCopy },
+}: {
+  copyValue: string | undefined;
+  copyProps: { tooltipCopy: number; tooltips: any; setTooltipCopy: Function };
+  copyIdx: number;
 }) => {
-    return <button
-    className="copy-btn"
-    data-clipboard-text={copyValue}
-    data-offset={window.innerWidth < 435? "{'left': 50}" : ""}
-    data-tip={tooltipCopy ? `Сopied to clipboard` : "Copy"}
-    ref={(node) => (tooltips.current[copyIdx] = node)}
-    onClick={() => {
-      setTooltipCopy(copyIdx);
-    }}
-  >
-    <img src={CopyIcon} alt="copy button" />
-  </button>
-}
- 
-export default CopyWithTooltip ;
+  return (
+    <button
+      className="copy-btn"
+      data-clipboard-text={copyValue}
+      data-offset={window.innerWidth < 435 ? "{'left': 50}" : ""}
+      data-tip={tooltipCopy ? `Сopied to clipboard` : "Copy"}
+      ref={(node) => (tooltips.current[copyIdx] = node)}
+      onClick={() => {
+        setTooltipCopy(copyIdx);
+      }}
+    >
+      <img className="ml-1" src={CopyIcon} alt="copy button" width={12} />
+    </button>
+  );
+};
+
+export default CopyWithTooltip;
