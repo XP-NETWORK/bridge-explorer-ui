57760ff3547b6d30d4b4a151370d63283c688238
Rishabh Kumar <rsbkme@gmail.com>
2022-03-13 17:57:57 +0530

    add chain icons to tools

--- src/constants.ts
+++ src/constants.ts
@@ -66,11 +66,11 @@ export const chains = [
   },
   { id: "iotex", name: "IOTEX", icon: "./assets/icons/iotex.svg" },
   { id: "fantom", name: "FANTOM", icon: "./assets/icons/fantom.svg" },
-  { id: "celo", name: "CELO", icon: "./assets/icons/celo.svg" },
+  // { id: "celo", name: "CELO", icon: "./assets/icons/celo.svg" },
   { id: "harmony", name: "HARMONY", icon: "./assets/icons/harmony.svg" },
   { id: "gnosis", name: "GNOSIS CHAIN", icon: "./assets/icons/gnosis.svg" },
   { id: "fuse-network-token", name: "FUSE", icon: "./assets/icons/fuse.svg" },
-  { id: "unique-one", name: "UNIQUE", icon: "./assets/icons/unique.svg" }, // TODO: check if this is correct
+  // { id: "unique-one", name: "UNIQUE", icon: "./assets/icons/unique.svg" }, // TODO: check if this is correct
   { id: "elrond-erd-2", name: "ELROND", icon: "./assets/icons/elrond.svg" },
 ];
 
