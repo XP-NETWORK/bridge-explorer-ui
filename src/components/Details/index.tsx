438a59fb49fabcc0c7e0a4389b14a4a0231e6eb9
Rishabh Kumar <rsbkme@gmail.com>
2022-03-14 16:18:17 +0530

    add back button

--- src/components/Details/index.tsx
+++ src/components/Details/index.tsx
@@ -1,22 +1,25 @@
-import * as React from 'react';
+import * as React from "react";
 import { Container } from "../Container";
+import { BackButton } from "./BackButton";
 
-import DetailsCard from './DetailsCard';
-import DetailsList from './DetailsList';
-import { withCopy } from './withCopy';
+import DetailsCard from "./DetailsCard";
+import DetailsList from "./DetailsList";
+import { withCopy } from "./withCopy";
 
-import { withData } from './withData';
+import { withData } from "./withData";
 
-export const EventDetailsSkeleton = withData(withCopy(({copyProps, data}) => {
-
-
-
-    return <div className="DetailsWrapper">
+export const EventDetailsSkeleton = withData(
+  withCopy(({ copyProps, data }) => {
+    return (
+      <div className="DetailsWrapper">
         <Container className="">
-        <div className="mt-10 bg-white rounded-2xl md:p-8 p-4 shadow-[0_1px_15px_0px_#2F303214] text-sm">
-            <DetailsCard copyProps={copyProps} data={data}/>
-            <DetailsList copyProps={copyProps} data={data}/>
-            </div>
+          <div className="mt-10 bg-white rounded-2xl md:p-8 p-4 shadow-[0_1px_15px_0px_#2F303214] text-sm">
+            <BackButton />
+            <DetailsCard copyProps={copyProps} data={data} />
+            <DetailsList copyProps={copyProps} data={data} />
+          </div>
         </Container>
-    </div>
-}))
+      </div>
+    );
+  })
+);
