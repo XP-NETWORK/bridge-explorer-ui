ea3f726444105df1ef383d3e43a7b1d45ccc7d8a
rony derra <derrarony@gmail.com>
2022-11-13 12:43:27 +0200

    hash config

--- src/components/ExplorerEvents.tsx
+++ src/components/ExplorerEvents.tsx
@@ -25,7 +25,7 @@ import { ChainListBox } from "../components/ChainModal/ChainListBox";
 import { DropDown } from "./elements/DropDown";
 import { CollectionNameRow } from "./Table/CollectionNameRow";
 import DownloadCSV from "./elements/CSVButton";
-
+import { confiUrl } from "../services/hashConfig";
 import {
   setEventsQueryString,
   setEventsQueryStringFrom,
@@ -81,12 +81,10 @@ export const ExplorerEvents: FC<{ status?: string }> = ({ status = "" }) => {
   const dispatch = useDispatch();
   const [showClearBtn, setShowClearBtn] = useState(false);
 
-  const { eventsQueryString, collectionName } = useSelector(
-    (state: ReduxState) => ({
-      eventsQueryString: state.global.eventsQueryString,
-      collectionName: state.global.showByCollection,
-    })
-  );
+  const { eventsQueryString, collectionName } = useSelector((state: ReduxState) => ({
+    eventsQueryString: state.global.eventsQueryString,
+    collectionName: state.global.showByCollection,
+  }));
 
   // useEffect(() => {
   //   dispatch(setStatusFilter(status));
@@ -95,10 +93,7 @@ export const ExplorerEvents: FC<{ status?: string }> = ({ status = "" }) => {
   let scrollBtn = useRef<any>(null);
 
   const scrollHandler = function () {
-    if (
-      window.innerHeight + window.scrollY >=
-      document.body.offsetHeight - 500
-    ) {
+    if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 500) {
       scrollBtn.current.style.bottom = "500px";
     } else {
       scrollBtn.current.style.bottom = "30px";
@@ -124,9 +119,7 @@ export const ExplorerEvents: FC<{ status?: string }> = ({ status = "" }) => {
     rates: { [key: string]: { usd: number } },
     chainName: string
   ): number => {
-    const chain = chains.find(
-      (chain) => chain.name.toLowerCase() === chainName.toLowerCase()
-    );
+    const chain = chains.find((chain) => chain.name.toLowerCase() === chainName.toLowerCase());
     const rate = (chain && rates[chain.id]?.usd) || 1;
 
     return rate;
@@ -134,8 +127,7 @@ export const ExplorerEvents: FC<{ status?: string }> = ({ status = "" }) => {
 
   useEffect(() => {
     if (
-      (typeof eventsQueryString === "object" &&
-        Object.keys(eventsQueryString).length > 0) ||
+      (typeof eventsQueryString === "object" && Object.keys(eventsQueryString).length > 0) ||
       collectionName !== ""
     ) {
       setShowClearBtn(true);
@@ -146,7 +138,7 @@ export const ExplorerEvents: FC<{ status?: string }> = ({ status = "" }) => {
     if (e.target.tagName.toLowerCase() === "img") {
       return;
     }
-    navigate(`/tx/${extractHash(event.fromHash)}`);
+    navigate(`/tx/${extractHash(confiUrl(event.fromHash))}`);
   };
 
   const handleClearAll = () => {
@@ -205,10 +197,7 @@ export const ExplorerEvents: FC<{ status?: string }> = ({ status = "" }) => {
             className="scrollTopBtn"
             ref={scrollBtn}
             onClick={(e) => {
-              setTimeout(
-                () => window.scrollTo({ top: 10, behavior: "smooth" }),
-                100
-              );
+              setTimeout(() => window.scrollTo({ top: 10, behavior: "smooth" }), 100);
             }}
           />
 
@@ -244,8 +233,7 @@ export const ExplorerEvents: FC<{ status?: string }> = ({ status = "" }) => {
               eventsContext?.events.length ? (
                 eventsContext?.events.map((event: IEvent, idx: number) => {
                   const dollarValue =
-                    getExchangeRate(exchangeRates, event.chainName) *
-                    formatFees(event);
+                    getExchangeRate(exchangeRates, event.chainName) * formatFees(event);
 
                   return (
                     <tr
@@ -255,23 +243,14 @@ export const ExplorerEvents: FC<{ status?: string }> = ({ status = "" }) => {
                     >
                       <TableData
                         className={`left-0 text-center bg-white group-hover:bg-[#F7F7F9] imgTableData ${
-                          /^((?!chrome|android).)*safari/i.test(
-                            navigator.userAgent
-                          )
+                          /^((?!chrome|android).)*safari/i.test(navigator.userAgent)
                             ? "safariHack"
                             : "sitckyBottomLine"
                         }`}
                       >
                         {" "}
-                        <a
-                          href={`/tx/${extractHash(event.fromHash)}`}
-                          target="_blank"
-                        >
-                          <ReactTooltip
-                            effect="solid"
-                            className="copyTip"
-                            multiline
-                          />
+                        <a href={`/tx/${extractHash(confiUrl(event.fromHash))}`} target="_blank">
+                          <ReactTooltip effect="solid" className="copyTip" multiline />
                           <RowNFT event={event} />
                         </a>{" "}
                       </TableData>
@@ -280,20 +259,14 @@ export const ExplorerEvents: FC<{ status?: string }> = ({ status = "" }) => {
                         <span
                           className="cursor-default"
                           data-tip={`
-                        ${formatFees(event)} ${
-                            event.fromChain && currency[event.fromChain]
-                          } <br>
+                        ${formatFees(event)} ${event.fromChain && currency[event.fromChain]} <br>
                           ${dollarValue} $
                       `}
                         >
                           <span>{formatFees(event).toFixed(7).toString()}</span>{" "}
-                          <span>
-                            {event.fromChain && currency[event.fromChain]}
-                          </span>
+                          <span>{event.fromChain && currency[event.fromChain]}</span>
                           <br />
-                          <span className="text-xs">
-                            ${dollarValue && dollarValue.toFixed(2)}
-                          </span>
+                          <span className="text-xs">${dollarValue && dollarValue.toFixed(2)}</span>
                         </span>
                       </TableData>
 
@@ -304,17 +277,13 @@ export const ExplorerEvents: FC<{ status?: string }> = ({ status = "" }) => {
                               chains.find(
                                 (chain) =>
                                   chain.name.toLowerCase() ===
-                                  chainNoncetoName[
-                                    event?.fromChain || 0
-                                  ]?.toLowerCase()
+                                  chainNoncetoName[event?.fromChain || 0]?.toLowerCase()
                               )?.icon
                             }
                             alt=""
                             className="chainIcon"
                           />
-                          <span>
-                            {chainNoncetoName[event?.fromChain || 0] || "N/A"}{" "}
-                          </span>
+                          <span>{chainNoncetoName[event?.fromChain || 0] || "N/A"} </span>
                         </div>
                         <ExplorerLink
                           hash={extractHash(event.fromHash!)}
@@ -329,23 +298,16 @@ export const ExplorerEvents: FC<{ status?: string }> = ({ status = "" }) => {
                               chains.find(
                                 (chain) =>
                                   chain.name.toLowerCase() ===
-                                  chainNoncetoName[
-                                    event?.toChain || 0
-                                  ]?.toLowerCase()
+                                  chainNoncetoName[event?.toChain || 0]?.toLowerCase()
                               )?.icon
                             }
                             alt=""
                             className="chainIcon"
                           />
-                          <span>
-                            {chainNoncetoName[event?.toChain || 0] || "N/A"}
-                          </span>
+                          <span>{chainNoncetoName[event?.toChain || 0] || "N/A"}</span>
                         </div>
                         {event?.status === "Completed" ? (
-                          <ExplorerLink
-                            hash={extractHash(event.toHash!)}
-                            chain={event.toChain!}
-                          />
+                          <ExplorerLink hash={extractHash(event.toHash!)} chain={event.toChain!} />
                         ) : event?.status === "Pending" ? (
                           <Loader className="addressLoader" />
                         ) : (
@@ -356,25 +318,19 @@ export const ExplorerEvents: FC<{ status?: string }> = ({ status = "" }) => {
                       <TableData className="CollectioName">
                         <CollectionNameRow
                           hash={extractHash(event.toHash!)}
-                          collectionName={
-                            event?.collectionName ? event?.collectionName : "-"
-                          }
+                          collectionName={event?.collectionName ? event?.collectionName : "-"}
                           chain={event.toChain!}
                         />
                       </TableData>
 
                       <TableData>
-                        <span className="methodDataTable">
-                          {event.type || "N/A"}
-                        </span>
+                        <span className="methodDataTable">{event.type || "N/A"}</span>
                       </TableData>
 
                       <TableData>
                         <span
                           className="valueData "
-                          data-tip={moment(event?.createdAt).format(
-                            "YYYY/MM/DD H:mm"
-                          )}
+                          data-tip={moment(event?.createdAt).format("YYYY/MM/DD H:mm")}
                         >
                           {moment(event.createdAt)
                             .fromNow()
@@ -412,10 +368,7 @@ export const ExplorerEvents: FC<{ status?: string }> = ({ status = "" }) => {
   );
 };
 
-export const TableHeading: FC<{ className?: string }> = ({
-  children,
-  className,
-}) => (
+export const TableHeading: FC<{ className?: string }> = ({ children, className }) => (
   <th
     scope="col"
     className={
@@ -427,14 +380,10 @@ export const TableHeading: FC<{ className?: string }> = ({
   </th>
 );
 
-export const TableData: FC<{ className?: string }> = ({
-  children,
-  className,
-}) => (
+export const TableData: FC<{ className?: string }> = ({ children, className }) => (
   <td
     className={
-      "px-3 py-4 min-w-[62px] border-0 whitespace-nowrap text-sm text-[#222222] " +
-      className
+      "px-3 py-4 min-w-[62px] border-0 whitespace-nowrap text-sm text-[#222222] " + className
     }
   >
     {children}
