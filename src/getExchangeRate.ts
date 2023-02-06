fb9ad0c54565a258cf19420182dd4f501fbca1e1
rony derra <derrarony@gmail.com>
2023-02-06 12:39:25 +0200

    getExchangeRates

--- src/getExchangeRate.ts
+++ src/getExchangeRate.ts
@@ -1,9 +1,14 @@
 export const getExchangeRates = async (ids: string[]) => {
-  const res = await fetch(
-    `https://api.coingecko.com/api/v3/simple/price?ids=${ids.join(
-      ","
-    )}&vs_currencies=usd`
-  );
-  
-  return await res.json();
+  try {
+    const res = await fetch(
+      `https://api.coingecko.com/api/v3/simple/price?ids=${ids.join(
+        ","
+      )}&vs_currencies=usd`
+    );
+
+    return await res.json();
+  } catch (error: any) {
+    console.log(error.message);
+    return undefined
+  }
 };
