37db4ba6b856ad5a898ceaab7853bb8e65cd61f6
YuraDXPNetwork <yurad@xp.network>
2023-04-03 07:45:09 +0300

    fixed crash after clearing the search bar

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -11,107 +11,110 @@ import { useNavigate } from "react-router-dom";
 import { ReduxState } from "../store";
 
 export const SearchBar: React.FC<{
-  mode: React.Dispatch<React.SetStateAction<boolean>>;
+    mode: React.Dispatch<React.SetStateAction<boolean>>;
 }> = ({ mode }) => {
-  const loc = useLocation();
-  const [value, setValue] = useState(
-    loc.pathname.replace("/", "") === "processing"
-      ? ""
-      : loc.pathname.replace("/", "")
-  );
+    const loc = useLocation();
+    const [value, setValue] = useState(
+        loc.pathname.replace("/", "") === "processing"
+            ? ""
+            : loc.pathname.replace("/", "")
+    );
 
-  const { eventsQueryString, from, to, statusFilter, resetSearch } =
-    useSelector((state: ReduxState) => ({
-      from: state.global.from,
-      to: state.global.to,
-      eventsQueryString: state.global.eventsQueryString,
-      statusFilter: state.global.statusFilter,
-      resetSearch: state.global.resetSearch,
-    }));
-  // const eventsQueryString = useSelector(
-  //   (state: ReduxState) => state.global.eventsQueryString
-  // );
+    const { eventsQueryString, from, to, statusFilter, resetSearch } =
+        useSelector((state: ReduxState) => ({
+            from: state.global.from,
+            to: state.global.to,
+            eventsQueryString: state.global.eventsQueryString,
+            statusFilter: state.global.statusFilter,
+            resetSearch: state.global.resetSearch,
+        }));
+    // const eventsQueryString = useSelector(
+    //   (state: ReduxState) => state.global.eventsQueryString
+    // );
 
-  const navigate = useNavigate();
-  const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
 
-  const debounced = useCallback(
-    debounce((value: string) => dispatch(setEventsQueryString(value)), 1000),
-    []
-  );
-  console.log({ eventsQueryString });
+    const debounced = useCallback(
+        debounce(
+            (value: string) => dispatch(setEventsQueryString(value)),
+            1000
+        ),
+        []
+    );
+    console.log({ eventsQueryString });
 
-  useEffect(() => {
-    if (resetSearch) {
-      mode(false);
-      setValue("");
-      // dispatch(setResetSearch(false)) ;
-    }
-  }, [resetSearch]);
+    useEffect(() => {
+        if (resetSearch) {
+            mode(false);
+            setValue("");
+            // dispatch(setResetSearch(false)) ;
+        }
+    }, [resetSearch]);
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    dispatch(setResetSearch(false));
-    e.target.value === "" ? handleClearSearch() : mode(true);
-    setValue(e.target.value);
-    navigate(e.target.value);
-  };
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        dispatch(setResetSearch(false));
+        e.target.value === "" ? handleClearSearch() : mode(true);
+        setValue(e.target.value);
+        navigate(e.target.value);
+    };
 
-  const handleClearSearch = () => {
-    mode(false);
-    setValue("");
-    navigate("/");
-  };
+    const handleClearSearch = () => {
+        mode(false);
+        setValue("");
+        // navigate("/");
+    };
 
-  useEffect(() => {
-    if (!resetSearch) {
-      if (value !== "") {
-        debounced(value);
-      }
-      value ? mode(true) : handleClearSearch();
-    }
-  }, [value]);
+    useEffect(() => {
+        if (!resetSearch) {
+            if (value !== "") {
+                debounced(value);
+            }
+            value ? mode(true) : handleClearSearch();
+        }
+    }, [value]);
 
-  // useEffect(() => {
-  //   window.addEventListener("beforeunload", alertUser);
-  //   return () => {
-  //     window.removeEventListener("beforeunload", alertUser);
-  //   };
-  // }, []);
-  // const alertUser = (e: any) => {
-  //   // e.preventDefault();
-  //   // e.returnValue = "";
-  //   handleClearSearch()
-  // };
+    // useEffect(() => {
+    //   window.addEventListener("beforeunload", alertUser);
+    //   return () => {
+    //     window.removeEventListener("beforeunload", alertUser);
+    //   };
+    // }, []);
+    // const alertUser = (e: any) => {
+    //   // e.preventDefault();
+    //   // e.returnValue = "";
+    //   handleClearSearch()
+    // };
 
-  return (
-    <Container className="mt-6">
-      <form
-        className="flex max-w-[635px] leading-9 mx-auto px-4 py-2 bg-white rounded shadow-[0_1px_15px_0px_#2F303214]"
-        onSubmit={(e) => {
-          e.preventDefault();
-        }}
-      >
-        <input
-          //type="search"
-          placeholder="Search address or chain name"
-          className="flex-1 bg-transparent focus:outline-none "
-          onChange={handleChange}
-          value={value}
-        />
-        <div
-          className="clearWrapper"
-          style={{ display: value ? "flex" : "none" }}
-        >
-          <img
-            src={Cross}
-            alt="cross"
-            width={12}
-            className="searchIcon clearSearch"
-            onClick={handleClearSearch}
-          />
-        </div>
-        <img src={SearchIcon} alt="search" className="searchIcon" />
-      </form>
-    </Container>
-  );
+    return (
+        <Container className="mt-6">
+            <form
+                className="flex max-w-[635px] leading-9 mx-auto px-4 py-2 bg-white rounded shadow-[0_1px_15px_0px_#2F303214]"
+                onSubmit={(e) => {
+                    e.preventDefault();
+                }}
+            >
+                <input
+                    //type="search"
+                    placeholder="Search address or chain name"
+                    className="flex-1 bg-transparent focus:outline-none "
+                    onChange={handleChange}
+                    value={value}
+                />
+                <div
+                    className="clearWrapper"
+                    style={{ display: value ? "flex" : "none" }}
+                >
+                    <img
+                        src={Cross}
+                        alt="cross"
+                        width={12}
+                        className="searchIcon clearSearch"
+                        onClick={handleClearSearch}
+                    />
+                </div>
+                <img src={SearchIcon} alt="search" className="searchIcon" />
+            </form>
+        </Container>
+    );
 };
--- src/components/elements/Сhart.tsx
+++ src/components/elements/Сhart.tsx
@@ -7,180 +7,225 @@ import { Loader } from "./Loader";
 import "./Chart.css";
 
 import {
-  Area,
-  AreaChart,
-  CartesianGrid,
-  ResponsiveContainer,
-  Tooltip,
-  XAxis,
-  YAxis,
+    Area,
+    AreaChart,
+    CartesianGrid,
+    ResponsiveContainer,
+    Tooltip,
+    XAxis,
+    YAxis,
 } from "recharts";
 export const Chart = withContainer(
-  ({
-    dailyData,
-    charFetching,
-    container: {
-      fetching,
-      appData: { totalTx },
-    },
-  }: {
-    dailyData: DailyData[];
-    charFetching: Boolean;
-    container: any;
-  }) => {
-    let mockData = [
-      // {
-      //   txNumber: 22,
-      //   walletsNumber: 3,
-      //   date: "2022/3/3",
-      // },
-      // {
-      //   txNumber: 7,
-      //   walletsNumber: 3,
-      //   date: "2022/3/4",
-      // },
-      // {
-      //   txNumber: 19,
-      //   walletsNumber: 3,
-      //   date: "2022/3/5",
-      // },
-      // {
-      //   txNumber: 56,
-      //   walletsNumber: 3,
-      //   date: "2022/3/6",
-      // },
-      // {
-      //   txNumber: 31,
-      //   walletsNumber: 3,
-      //   date: "2022/3/7",
-      // },
-      ...dailyData,
-    ];
+    ({
+        dailyData,
+        charFetching,
+        container: {
+            fetching,
+            appData: { totalTx },
+        },
+    }: {
+        dailyData: DailyData[];
+        charFetching: Boolean;
+        container: any;
+    }) => {
+        let mockData = [
+            // {
+            //   txNumber: 22,
+            //   walletsNumber: 3,
+            //   date: "2022/3/3",
+            // },
+            // {
+            //   txNumber: 7,
+            //   walletsNumber: 3,
+            //   date: "2022/3/4",
+            // },
+            // {
+            //   txNumber: 19,
+            //   walletsNumber: 3,
+            //   date: "2022/3/5",
+            // },
+            // {
+            //   txNumber: 56,
+            //   walletsNumber: 3,
+            //   date: "2022/3/6",
+            // },
+            // {
+            //   txNumber: 31,
+            //   walletsNumber: 3,
+            //   date: "2022/3/7",
+            // },
+            ...dailyData,
+        ];
 
-    mockData = mockData.map((item, i) => ({
-      ...item,
-      adate: new Date(item.date).getTime(),
-      idx: i,
-    }));
+        mockData = mockData.map((item, i) => ({
+            ...item,
+            adate: new Date(item.date).getTime(),
+            idx: i,
+        }));
 
-    console.log({mockData});
-    
-    const CustomTooltip = ({ active, payload, label }: any) => {
-      if (active && payload && payload.length) {
-        console.log(payload[0]);
+        console.log({ mockData });
+
+        const CustomTooltip = ({ active, payload, label }: any) => {
+            if (active && payload && payload.length) {
+                console.log(payload[0]);
+                return (
+                    <div className="tooltipDiv">
+                        <p className="dateTool">{payload[0].payload.date}</p>
+                        {payload[0].value} Tx
+                    </div>
+                );
+            }
+
+            return null;
+        };
+
         return (
-          <div className="tooltipDiv">
-            <p className="dateTool">{payload[0].payload.date}</p>
-            {payload[0].value} Tx
-          </div>
-        );
-      }
-
-      return null;
-    };
-
-    return (
-      <div className="lg:max-w-5xl mx-auto px-4 mt-3 md:mt-6">
-        <div className="chartWrapper">
-          <div className="chartVisual">
-            <h3 className="font-medium text-[#222222]">Daily Transactions</h3>
-            <div className="chartMetrics">
-              <span>
-                Today Tx:{" "}
-                <span>
-                  {fetching ? (
-                    <Loader />
-                  ) : (
-                    mockData[mockData.length - 1].Tx
-                  )}
-                </span>
-              </span>
-              <span>
-                Total Tx: <span>{fetching ? <Loader /> : totalTx}</span>
-              </span>
-            </div>
-            <div className="lineWrapper">
-              {charFetching ? (
-                <div className="chartLoaderWrap">
-                  <span className="super-loader"></span>
+            <div className="lg:max-w-5xl mx-auto px-4 mt-3 md:mt-6">
+                <div className="chartWrapper">
+                    <div className="chartVisual">
+                        <h3 className="font-medium text-[#222222]">
+                            Daily Transactions
+                        </h3>
+                        <div className="chartMetrics">
+                            <span>
+                                Today Tx:{" "}
+                                <span>
+                                    {fetching ? (
+                                        <Loader />
+                                    ) : (
+                                        mockData[mockData.length - 1]?.Tx
+                                    )}
+                                </span>
+                            </span>
+                            <span>
+                                Total Tx:{" "}
+                                <span>{fetching ? <Loader /> : totalTx}</span>
+                            </span>
+                        </div>
+                        <div className="lineWrapper">
+                            {charFetching ? (
+                                <div className="chartLoaderWrap">
+                                    <span className="super-loader"></span>
+                                </div>
+                            ) : (
+                                <ResponsiveContainer width="95%" height={251}>
+                                    <AreaChart
+                                        data={mockData}
+                                        margin={{
+                                            top: 10,
+                                            right: 30,
+                                            left: 0,
+                                            bottom: 0,
+                                        }}
+                                    >
+                                        <defs>
+                                            <linearGradient
+                                                id="colorUv"
+                                                x1="0"
+                                                y1="0"
+                                                x2="0"
+                                                y2="1"
+                                            >
+                                                <stop
+                                                    offset="5%"
+                                                    stopColor="#5B8FF9"
+                                                    stopOpacity={0.25}
+                                                />
+                                                <stop
+                                                    offset="95%"
+                                                    stopColor="#5B8FF9"
+                                                    stopOpacity={0.25}
+                                                />
+                                            </linearGradient>
+                                        </defs>
+                                        {/* <XAxis dataKey="day" tickMargin={4} label="mar"/> */}
+                                        <YAxis
+                                            axisLine={false}
+                                            type="number"
+                                            domain={[
+                                                (dataMin: any) => 0,
+                                                (dataMax: any) =>
+                                                    Math.round(
+                                                        dataMax / 100 + 1
+                                                    ) *
+                                                        100 +
+                                                    500,
+                                            ]}
+                                        />
+                                        <CartesianGrid
+                                            strokeDasharray="1"
+                                            vertical={false}
+                                        />
+                                        <Tooltip content={<CustomTooltip />} />
+                                        <Area
+                                            type="monotone"
+                                            dataKey="Tx"
+                                            stroke="#2E66F5"
+                                            fillOpacity={1}
+                                            fill="url(#colorUv)"
+                                        />
+                                    </AreaChart>
+                                </ResponsiveContainer>
+                            )}
+                        </div>
+                    </div>
+                    {false && (
+                        <div className="chartRates">
+                            <div className="periodButtons">
+                                <span className="text-[#222222]">Today</span>
+                                <span className="text-[#222222]">
+                                    Last 7 days
+                                </span>
+                                <span className="text-[#222222]">
+                                    All period
+                                </span>
+                            </div>
+                            <ul className="chartInfoList">
+                                <li>
+                                    <span className="chartItemName font-medium">
+                                        Volume
+                                    </span>
+                                    <span className="chartItemValue">
+                                        $324.34 M
+                                    </span>
+                                </li>
+                                <li>
+                                    <span className="chartItemName font-medium">
+                                        Fees
+                                    </span>
+                                    <span className="chartItemValue">
+                                        $324.34 M
+                                    </span>
+                                </li>
+                                <li>
+                                    <span className="chartItemName font-medium">
+                                        Users
+                                    </span>
+                                    <span className="chartItemValue">
+                                        $324.34 M
+                                    </span>
+                                </li>
+                                <li>
+                                    <span className="chartItemName font-medium">
+                                        Tx
+                                    </span>
+                                    <span className="chartItemValue">
+                                        $324.34 M
+                                    </span>
+                                </li>
+                                <li>
+                                    <span className="chartItemName font-medium">
+                                        Avg Tx time
+                                    </span>
+                                    <span className="chartItemValue">
+                                        213.12s/2.12m
+                                    </span>
+                                </li>
+                            </ul>
+                        </div>
+                    )}
                 </div>
-              ) : (
-                <ResponsiveContainer width="95%" height={251}>
-                  <AreaChart
-                    data={mockData}
-                    margin={{ top: 10, right: 30, left: 0, bottom: 0 }}
-                  >
-                    <defs>
-                      <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
-                        <stop
-                          offset="5%"
-                          stopColor="#5B8FF9"
-                          stopOpacity={0.25}
-                        />
-                        <stop
-                          offset="95%"
-                          stopColor="#5B8FF9"
-                          stopOpacity={0.25}
-                        />
-                      </linearGradient>
-                    </defs>
-                    {/* <XAxis dataKey="day" tickMargin={4} label="mar"/> */}
-                    <YAxis
-                      axisLine={false}
-                      type="number"
-                      domain={[
-                        (dataMin: any) => 0,
-                        (dataMax: any) =>
-                          Math.round(dataMax / 100 + 1) * 100 + 500,
-                      ]}
-                    />
-                    <CartesianGrid strokeDasharray="1" vertical={false} />
-                    <Tooltip content={<CustomTooltip />} />
-                    <Area
-                      type="monotone"
-                      dataKey="Tx"
-                      stroke="#2E66F5"
-                      fillOpacity={1}
-                      fill="url(#colorUv)"
-                    />
-                  </AreaChart>
-                </ResponsiveContainer>
-              )}
             </div>
-          </div>
-          {false && (
-            <div className="chartRates">
-              <div className="periodButtons">
-                <span className="text-[#222222]">Today</span>
-                <span className="text-[#222222]">Last 7 days</span>
-                <span className="text-[#222222]">All period</span>
-              </div>
-              <ul className="chartInfoList">
-                <li>
-                  <span className="chartItemName font-medium">Volume</span>
-                  <span className="chartItemValue">$324.34 M</span>
-                </li>
-                <li>
-                  <span className="chartItemName font-medium">Fees</span>
-                  <span className="chartItemValue">$324.34 M</span>
-                </li>
-                <li>
-                  <span className="chartItemName font-medium">Users</span>
-                  <span className="chartItemValue">$324.34 M</span>
-                </li>
-                <li>
-                  <span className="chartItemName font-medium">Tx</span>
-                  <span className="chartItemValue">$324.34 M</span>
-                </li>
-                <li>
-                  <span className="chartItemName font-medium">Avg Tx time</span>
-                  <span className="chartItemValue">213.12s/2.12m</span>
-                </li>
-              </ul>
-            </div>
-          )}
-        </div>
-      </div>
-    );
-  }
+        );
+    }
 );
