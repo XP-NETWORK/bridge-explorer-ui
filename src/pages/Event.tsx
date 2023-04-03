b216e323c5942dd6083177d8800967012fbc40a1
YuraDXPNetwork <yurad@xp.network>
2023-04-03 11:01:52 +0300

    small ui fixes and google analytics added

--- src/pages/Event.tsx
+++ src/pages/Event.tsx
@@ -1,18 +1,18 @@
 import { Footer } from "../components/Footer";
 import { Navbar } from "../components/Navbar";
-import { EventDetails } from "../components/EventDetails";
 import { EventDetailsSkeleton } from "../components/Details";
 import { useEffect } from "react";
+import { handleGoogleAnalyticsPageView } from "../GA4";
 
 export const Event = () => {
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, []);
-  return (
-    <div>
-      <Navbar />
-      <EventDetailsSkeleton />
-      <Footer />
-    </div>
-  );
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, []);
+    return (
+        <div>
+            <Navbar />
+            <EventDetailsSkeleton />
+            <Footer />
+        </div>
+    );
 };
